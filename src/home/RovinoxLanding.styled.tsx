a2b4bf43f49020bf6878dc439f70a7701e0264b0
Yusef <yusefcodes@gmail.com>
2022-10-08 14:39:04 -0500

    changes site links

---
+++ src/home/RovinoxLanding.styled.tsx
@@ -0,0 +1,18 @@
+import styled from "@emotion/styled";
+
+export const RovinoxTitle = styled.h2`
+    margin-left: 40px;
+    font-family: monospace;
+    font-size: 36px;
+    font-weight: 700px;
+    letter-spacing: 0.3rem;
+
+    span {
+        color: #EC572C;
+    }
+`
+
+export const CoursePromise = styled.p`
+    display: flex;
+    align-items: center;
+`
